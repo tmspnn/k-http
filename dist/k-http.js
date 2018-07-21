04a4b8e92a15e2bd3f3c6871e95ea3d135aeba4e
tmspnn <tmspnn@gmail.com>
2018-07-21 11:18:57 +0800

    0.2.1

--- dist/k-http.js
+++ dist/k-http.js
@@ -10,24 +10,24 @@ exports.default = {
         return k_xhr_1.default(xhrOptions).then(function (res) { return (jsonLike(res) ? JSON.parse(res) : res); });
     },
     post: function (url, data, options) {
-        return send(url, 'post', data, options);
+        return send(url, "post", data, options);
     },
     put: function (url, data, options) {
-        return send(url, 'put', data, options);
+        return send(url, "put", data, options);
     },
     del: function (url, options) {
-        return k_xhr_1.default(Object.assign({ url: url, method: 'delete' }, options)).then(function (res) { return (jsonLike(res) ? JSON.parse(res) : res); });
+        return k_xhr_1.default(Object.assign({ url: url, method: "delete" }, options)).then(function (res) { return (jsonLike(res) ? JSON.parse(res) : res); });
     }
 };
 function jsonLike(s) {
     var char0 = s.charAt(0);
-    return char0 == '{' || char0 == '[';
+    return char0 == "{" || char0 == "[";
 }
 function send(url, method, data, options) {
     var xhrOptions = Object.assign({ url: url, method: method }, options);
     if (data instanceof Object && data.constructor == Object) {
         Object.assign(xhrOptions, {
-            contentType: 'application/json',
+            contentType: "application/json",
             data: JSON.stringify(data)
         });
     }
